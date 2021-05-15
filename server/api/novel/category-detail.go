4b0dfdf8279aa2caf9581d2f8b858403f63101cc
chengche123 <1107640282@qq.com>
2021-05-15 18:04:38 +0800

    增加轻小说分类详情api

---
+++ server/api/novel/category-detail.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	rapi "crawler/server/api"
+	"strconv"
+	"strings"
+)
+
+/*
+/// 轻小说类目详情
+static String novelCategoryDetail(
+	{int cateId = 0, int status = 0, int sort = 0, int page = 0}) {
+return "$apiHost/novel/$cateId/$status/$sort/$page.json?${defaultParameter()}";
+}
+*/
+
+func c(i int) string {
+	return strconv.Itoa(i)
+}
+
+// status为进度id,可以在NovelCategory表里找
+func NovelCategoryDetail(id, status, sort, page int) string {
+	params := []string{c(id), c(status), c(sort), c(page)}
+
+	param := strings.Join(params, "/")
+
+	return rapi.Host + "/novel/" + param + ".json"
+}
---
+++ server/api/novel/category-detail_test.go
@@ -0,0 +1,7 @@
+package api
+
+import "testing"
+
+func TestNovelCategoryDetail(t *testing.T) {
+	t.Log(NovelCategoryDetail(8, 0, 0, 0))
+}
