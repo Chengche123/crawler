916789c6e3579ac58d71e703b6a443c7038e0c3f
chengche123 <1107640282@qq.com>
2021-05-16 11:25:14 +0800

    抽象出downloader

---
+++ share/download/downloader.go
@@ -0,0 +1,45 @@
+package download
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Repository interface {
+	FindStaticUrls() (<-chan []string, error)
+}
+
+type Downloader struct {
+	Repository Repository
+}
+
+func (d *Downloader) StartDownload() error {
+	inchan, err := d.Repository.FindStaticUrls()
+	if err != nil {
+		return fmt.Errorf("failed to find models in dao: %v", err)
+	}
+
+	const concur = 8
+	var wg sync.WaitGroup
+
+	for i := 0; i < concur; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for urls := range inchan {
+				err := DownloadStatic(urls)
+				if err != nil {
+					// log啥?
+					continue
+				}
+
+			}
+
+		}()
+	}
+
+	wg.Wait()
+
+	return nil
+}
