2e1595c06e0a613824ec9fc62d4587b31a358c48
chengche123 <1107640282@qq.com>
2021-07-28 19:13:07 +0800

    cors

--- cmd/file-server/main.go
+++ cmd/file-server/main.go
@@ -6,5 +6,11 @@ import (
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":80", http.FileServer(http.Dir("./static"))))
+	h := http.FileServer(http.Dir(`D:\final\Projects\flutter_comic\build\web`))
+	log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Access-Control-Allow-Origin", "*")
+		rw.Header().Add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")
+
+		h.ServeHTTP(rw, r)
+	})))
 }
